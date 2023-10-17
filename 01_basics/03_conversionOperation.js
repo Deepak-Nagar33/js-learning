703c810b4a7aa83766277c9153cecef80a6cf752
Deepak Nagar <143583403+Deepak-Nagar33@users.noreply.github.com>
2023-10-17 07:57:51 +0000

    updo

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,29 +1,58 @@
-let score = "30abc"
-// const {score }= req.body
-console.log(typeof score);
-console.log(typeof (score));
+// let score = "30abc"
+// // const {score }= req.body
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
 
-console.log(valueInNumber);
+// console.log(valueInNumber);
 
-/* when we convert anumber "33"= 33 it will easily convert
-but when we convert "33abc" = NaN we'll get this  not a number
+// /* when we convert anumber "33"= 33 it will easily convert
+// but when we convert "33abc" = NaN we'll get this  not a number
 
-true = 1; false =0
+// true = 1; false =0
 
-*/
- let isLoggedIn = ""
- let booleanIsLoggedIn = Boolean(isLoggedIn)
- console.log(booleanIsLoggedIn);
-// 1= true ;
-//  0 = false 
-// "" = false 
-// "deepak" = true
+// */
+//  let isLoggedIn = ""
+//  let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+// // 1= true ;
+// //  0 = false 
+// // "" = false 
+// // "deepak" = true
 
 
-let someNumber = 33
-let stringNumber =  String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+
+
+
+
+
+// let someNumber = 33
+// let stringNumber =  String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***************************operations*********************
+
+// let value =3
+// let negvalue = -value
+// // console.log(value);
+
+// let str1 = "hello"
+// let str2 = " Deepak"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+
+// // ulta karne pr 
+// console.log(1+2+"2");
+
+// console.log(+true)
+let gameCounter = 100
+gameCounter++//postfix
+++gameCounter //prefix
+console.log(gameCounter);
