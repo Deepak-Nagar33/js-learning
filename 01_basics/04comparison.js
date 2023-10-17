2c5873eae83c90136e16267c59d9b218b03e500f
Deepak Nagar <143583403+Deepak-Nagar33@users.noreply.github.com>
2023-10-17 08:14:20 +0000

    conparison

---
+++ 01_basics/04comparison.js
@@ -0,0 +1,21 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+console.log(null > 0);//false
+console.log(null == 0);//false
+console.log(null >= 0);//true its because comparison convert null to number treating it as 0
+ console.log(undefined ==0);
+ console.log(undefined > 0);
+ console.log(undefined < 0);
+ console.log(undefined >= 0);
+ //all will false in undefined
+
+
+ // ===
+//  strict check
+
+console.log("2"===2);
