2b0801d194d3d71c98d8bf67b8e9dd8767cc496f
Deepak Nagar <143583403+Deepak-Nagar33@users.noreply.github.com>
2023-10-17 08:33:42 +0000

    DataTypeSummery

---
+++ 01_basics/05dataTypesSummery.js
@@ -0,0 +1,30 @@
+// IS JS DYNAMIC  TYPED OR STATIC TYPED ?
+
+//primitive Data Type
+
+/* 7 Types : String,
+             Number,
+             boolean
+             null
+             undefined
+             Symbol
+             BigInt 
+
+
+
+reference Data Type(non primitive)
+
+    array
+    objects 
+    Function
+*/
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail ;
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
