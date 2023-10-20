c493e526a371a48ad94e8f7e29e66c59d4ab5d77
Deepak Nagar <143583403+Deepak-Nagar33@users.noreply.github.com>
2023-10-20 08:17:41 +0000

    arrayBasic

---
+++ 02arrays/01arr.js
@@ -0,0 +1,15 @@
+//arrays
+/*
+javaScript array-copy operations 
+ shallow copies: 
+ (A shallow copy of an object is a copy whose 
+properties share the same references (point ),
+ 
+deep copies:
+(A deep copy of an object is a copy
+ whose properties do not share the same references (point)).
+*/
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeros = ["shaktiman", "nagraj"]
+
+console.log(myArr[0]);
